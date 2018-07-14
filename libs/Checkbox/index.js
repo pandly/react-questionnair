572d115253d8e777df89be8890a43903daa1895a
Shen Hao <haleshen@163.com>
2018-07-14 15:32:26 +0800

    try to modify jsx format

--- libs/Checkbox/index.js
+++ libs/Checkbox/index.js
@@ -2,45 +2,45 @@ import React from 'react';
 import './index.less';
 
 class Checkbox extends React.PureComponent {
-	static defaultProps = {
-		value: '',
-		name: '',
-	}
+  static defaultProps = {
+    value: '',
+    name: '',
+  }
 
-	handleChange = (e) => {
-		const {
-			onChange,
-			index,
-		} = this.props;
-		if (onChange) {
-			onChange(e, index);
-		};
-	}
+  handleChange = (e) => {
+    const {
+      onChange,
+      index,
+    } = this.props;
+    if (onChange) {
+      onChange(e, index);
+    };
+  }
 
-	render() {
-		const {
-			defaultChecked,
-			value,
-			name,
-			index,
-			label,
-			style,
-		} = this.props;
-		return (
-			<label className="wowjoy-checkbox" style={style}>
-				<input 
-				  type="checkbox"
-				  name={name}
-				  value={value}
-				  data-index={index}
-				  defaultChecked={defaultChecked}
-				  onChange={this.handleChange}
-				  style={{ display: 'none' }}/>
-						<span className="wowjoy-checkbox__inner"></span>
-			    	<span className="wowjoy-checkbox__text">{label}</span>
-			</label>
-		);
-	}
+  render() {
+    const {
+      defaultChecked,
+      value,
+      name,
+      index,
+      label,
+      style,
+    } = this.props;
+    return (
+      <label className="wowjoy-checkbox" style={style}>
+        <input
+          type="checkbox"
+          name={name}
+          value={value}
+          data-index={index}
+          defaultChecked={defaultChecked}
+          onChange={this.handleChange}
+          style={{ display: 'none' }}/>
+            <span className="wowjoy-checkbox__inner"></span>
+            <span className="wowjoy-checkbox__text">{label}</span>
+      </label>
+    );
+  }
 }
 
 export default Checkbox;
