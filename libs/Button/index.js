9647903f9008d14854db3570d5d5a887487b8e21
Shen Hao <haleshen@163.com>
2018-07-14 15:30:29 +0800

    try to modify jsx format

--- libs/Button/index.js
+++ libs/Button/index.js
@@ -2,41 +2,41 @@ import React from 'react';
 import './index.less';
 
 class Button extends React.PureComponent {
-	static defaultProps = {
-		disabled: false,
-		type: '',
-		size: '',
-	}
+  static defaultProps = {
+    disabled: false,
+    type: '',
+    size: '',
+  }
 
-	handleClick = (e) => {
-		const {
-			onClick,
-		} = this.props;
-		if (onClick) {
-			onClick();
-		};
-	}
+  handleClick = (e) => {
+    const {
+      onClick,
+    } = this.props;
+    if (onClick) {
+      onClick();
+    };
+  }
 
-	render() {
-		const {
-			type,
-			size,
-			disabled,
-			children,
-			onClick,
-			...otherProps,
-		} = this.props;
-		const buttonType = type ? `wowjoy-button__${type}` : '';
-		const buttonSize = size ? `wowjoy-button__${size}` : '';
-		return (
-			<button 
+  render() {
+    const {
+      type,
+      size,
+      disabled,
+      children,
+      onClick,
+      ...otherProps,
+    } = this.props;
+    const buttonType = type ? `wowjoy-button__${type}` : '';
+    const buttonSize = size ? `wowjoy-button__${size}` : '';
+    return (
+      <button
         className={`wowjoy-button ${buttonType} ${buttonSize}`}
         disabled={disabled}
         onClick={this.handleClick}>
           {children}
       </button>
-		);
-	}
+    );
+  }
 }
 
 export default Button;
